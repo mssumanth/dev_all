a793d7e31ef1cd4f2d48632bbc110efdbd825203
Lucy Park <me@lucypark.kr>
2024-04-07 09:36:54 -0700

    Replace api keys

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from openai import OpenAI  # openai>=1.2.0
 import requests
 
-solar_api_key = "SOLAR_API_KEY"
-docai_api_key = "DOCAI_API_KEY"
+docai_api_key = "hack-with-upstage-docai-0407"
+solar_api_key = "hack-with-upstage-solar-0407"
 
 client = OpenAI(
     api_key=solar_api_key,
@@ -11,7 +11,7 @@ client = OpenAI(
 
 def analyze_layout(filename):
     url = "https://api.upstage.ai/v1/document-ai/layout-analyzer"
-    headers = {"Authorization": f"Bearer {upstage_api_key}"}
+    headers = {"Authorization": f"Bearer {docai_api_key}"}
     files = {"document": open(filename, "rb")}
     response = requests.post(url, headers=headers, files=files)
     return response.json()["html"]
@@ -29,12 +29,12 @@ def ask_solar(context, question):
     )
     return response.choices[0].message.content
 
-def check_groundedness(context, answer):
+def check_groundedness(context, question, answer):
     response = client.chat.completions.create(
         model="solar-1-mini-answer-verification",
         messages=[
             {"role": "user", "content": context},
-            {"role": "assistant", "content": answer}
+            {"role": "assistant", "content": question + answer}
         ]
     )
     return response.choices[0].message.content == "grounded"
@@ -43,7 +43,7 @@ context = analyze_layout("upstage.png")
 question = "When was Upstage founded?"
 for _ in range(3):
     answer = ask_solar(context, question)
-    grounded = check_groundedness(context, answer)
+    grounded = check_groundedness(context, question, answer)
     if grounded:
         print(answer)
         break
